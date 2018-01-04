7bcb11a3fc679695503c53ae6daabdcea5531511
segalb <none>
2018-01-04 18:38:01 -0500

    Minor todo Fixes

--- todo.js
+++ todo.js
@@ -1,2 +1,1 @@
-2. //TODO: Organize Profile page - Populate name of organizer and organize css cards
-3. //TODO: Organize all the underline - text-decoration:none;
+1. //TODO: Organize all the underline - text-decoration:none;
