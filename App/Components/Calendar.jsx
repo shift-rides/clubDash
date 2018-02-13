cfafc957a7cdc01649abf751ba0f329db962aa1a
segalb <none>
2018-02-13 03:17:25 -0500

    minor change

--- App/Components/Calendar.jsx
+++ App/Components/Calendar.jsx
@@ -306,7 +306,7 @@ class Calendar extends React.Component {
           {...this.props}
           events={this.state.eventList}
           views={['week', 'day']}
-          step={60}
+          step={30}
           defaultDate={new Date(Date.now())}
           min={new Date(today.getFullYear(), today.getMonth(), today.getDate(), 6)}
           defaultView='week'
