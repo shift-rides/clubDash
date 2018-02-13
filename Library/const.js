906d5dd07e0a9b74a6b4315a13da8ae22e9d91c9
segalb <none>
2018-02-13 03:11:17 -0500

    final

--- Library/const.js
+++ Library/const.js
@@ -1,6 +1,6 @@
 exports.AVAILABLE_NUMBERS = ['1', '2', '3', '4', '5', '6', '7', '8']
-exports.ORIGINS = ['Brandeis', 'Logan Airport', 'New Jersey', 'New York']
-exports.DESTINATIONS = ['Brandeis', 'Logan Airport', 'New Jersey', 'New York']
+exports.ORIGINS = ['Brandeis',  'Logan Airport', 'South Station', 'New York', 'New Jersey']
+exports.DESTINATIONS = ['Brandeis', 'Logan Airport', 'South Station', 'New York', 'New Jersey']
 exports.DUMMY_EVENTS = [
   {desc: 'Trip to New York', organizer: 'Ben'},
   {desc: 'Trip to Logan', organizer: 'Akiba'},
--- todo.js
+++ todo.js
@@ -1,2 +1,8 @@
 1. //TODO: Organize all the underline - text-decoration:none;
 123
+
+
+//those are the dependancy thats slow down under pakage.
+"dev": "concurrently -k \"npm run devWebpack\" \"npm run devServer\"",
+"devWebpack": "webpack --watch",
+"devServer": "node ./Server/server.js"
