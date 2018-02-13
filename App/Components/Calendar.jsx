9829f5fcb2a2d4acda8a1c3cbe4836b540f6e7d8
segalb <none>
2018-02-13 03:28:15 -0500

    final test

--- App/Components/Calendar.jsx
+++ App/Components/Calendar.jsx
@@ -308,7 +308,7 @@ class Calendar extends React.Component {
           views={['week', 'day']}
           step={60}
           defaultDate={new Date(Date.now())}
-          min={new Date(today.getFullYear(), today.getMonth(), today.getDate(), 12)}
+          // min={new Date(today.getFullYear(), today.getMonth(), today.getDate(), 12)}
           defaultView='week'
           selectable
           onSelectEvent={(e) => this.handleOnSelectEvent(e)}
